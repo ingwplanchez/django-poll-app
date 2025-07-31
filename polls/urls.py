c64197aa918cadfbf62c17253d5c5da800a54434
Wilmer Planchez <ing.wplanchez@gmail.com>
2025-07-31 19:19:43 -0400

    Modified: Se agrega un espacio de nombres a la aplicación

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
     # ex: /polls/
     path("", views.index, name="index"),
@@ -12,3 +14,4 @@ urlpatterns = [
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
+
